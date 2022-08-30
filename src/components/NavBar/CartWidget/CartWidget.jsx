41c393ea6228af949e0b0720e6b5746386a8a18b
robertostorino <roberguitarsoul@gmail.com>
2022-08-30 16:18:35 -0300

    created Landing + refined NavBar

---
+++ src/components/NavBar/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import cart from "../../../assets/images/cart.png";
+
+const CartWidget = () => {
+    return (
+        <img src={cart} alt="cart" />
+    )
+}
+
+export default  CartWidget;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,38 @@
 import React from 'react'
 import './NavBar.css'
+import CartWidget from './CartWidget/CartWidget'
 import logo from '../../assets/images/logo.png'
 
 
 const NavBar = () => {
     return (
-        <header className="container py-3 sticky-md-top color_grey rounded-pill">
-            <div className="row ">
-                <div className="col-md-11">
-                    <ul className="nav d-flex align-items-center">
+        <nav className="navbar navbar-expand-lg">
+            <div className="container py-2 sticky-md-top color_grey rounded">
+                <a className="navbar-brand" href="./index.html"><img src={logo} alt="Play 4 win" width="58" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" ariaCurrent="page" href="./index.html"><img src={logo} alt="Play 4 win" width="58" /></a>
+                            <a className="nav-link active underline-hover" aria-current="page" href="./index.html">Games</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active underline-hover fs-3" href="./index.html">Games</a>
+                            <a className="nav-link underline-hover" href="#">Accesories</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link underline-hover fs-3" href="#">Accesories</a>
+                            <a className="nav-link underline-hover" href="#">PS4</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link underline-hover fs-3" href="#">PS4</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link underline-hover fs-3" href="#">PS5</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link underline-hover fs-3" href="#">Contact</a>
+                            <a className="nav-link underline-hover" href="#">PS5</a>
                         </li>
                     </ul>
                 </div>
+                <div>
+                    <CartWidget />
+                </div>
             </div>
-        </header>
+        </nav>
     )
 }
 
