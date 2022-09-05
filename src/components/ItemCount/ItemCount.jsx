63855f00e91e61c331c7730e80052fc293b1bfde
robertostorino <roberguitarsoul@gmail.com>
2022-09-05 20:40:36 -0300

    mejorando el código de ItemCount.jsx

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,27 +1,20 @@
-import React from 'react'
-import { useState } from 'react';
+import React,{ useState } from 'react'
 
 //intial: es la cantidad de ítems. Sirve para inicializar el contador en 1 por defecto
 
 
-const ItemCount = ({stock, initial, onAdd}) => {
-    
+const ItemCount = (props) => {
+
     //      Hooks
-    const [amount, setAmount] = useState(initial);
-    const [itemStock, setItemStock] = useState(stock);
-    const [itemAdd, setItemAdd] = useState(onAdd);
+    const [amount, setAmount] = useState(props.initial);
+    const [itemStock, setItemStock] = useState(props.stock);
+    const [itemAdd, setItemAdd] = useState(props.onAdd);
 
-    //      Restrictions
-
-    //decrementa solo si value es mayor a 0
-    const decrementAmount = (value) => {
-        (value > 0) && setAmount(value);
-    }
-
-    //incremento si no me paso del stock
+    //incrementa
     const incrementAmount = (value) => {
-        (value <= itemStock) && setAmount(value);
+        setAmount(amount + value);
     }
+
 
     //Agrega a Selected Products si amount es menor o igual al stock del item
     const addProducts = () => {
@@ -41,17 +34,19 @@ const ItemCount = ({stock, initial, onAdd}) => {
                             type="button" 
                             className="btn btn-secondary" 
                             value="-" 
-                            onClick={() => {decrementAmount(amount-1)}}/>
+                            onClick={() => {incrementAmount(-1)}}
+                            disabled={amount === props.initial}/>
                         <input 
                             type="text" 
                             className="form-control text-center"  
-                            value={amount} 
+                            value={amount}
                             onChange={() => {}} />
                         <input 
                             type="button" 
                             className="btn btn-secondary" 
                             value="+" 
-                            onClick={() => {incrementAmount(amount+1)}}/>
+                            onClick={() => {incrementAmount(+1)}}
+                            disabled={amount === props.stock}/>
                     </div>
                     <div className="d-grid gap-2 py-2">
                         <input 
