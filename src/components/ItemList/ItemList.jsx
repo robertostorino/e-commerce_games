e7ee6dd675750b898f4dc71190f41bfd9c097567
robertostorino <roberguitarsoul@gmail.com>
2022-09-05 23:43:56 -0300

    cargo estructura de carpetas

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemList = () => {
+    return{
+
+    }
+}
+
+export default ItemList;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ItemList from '../ItemList/ItemList';
 import './ItemListContainer.css'
 
 const ItemListContainer = ({greeting}) => {
     return (
         <div>
             <h1 className="text-center text-blue">{greeting}</h1>
+            <ItemList />
         </div>
     )
 }
